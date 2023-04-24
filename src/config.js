b649d706088c193bc9fc855d9770c66f31f81389
r.belov <r.belov@kck.ru>
2023-04-24 12:20:27 +0300

    Добавление сбилженого проекта

--- src/config.js
+++
@@ -1,2 +0,0 @@
-// export const API_BASE_URL = "https://vue-study.skillbox.cc";
-export const API_BASE_URL = "https://vue-tzr.skillbox.cc";
--- src/helpers/typesProps.js
+++
@@ -1,8 +0,0 @@
-export default Object.freeze({
-  built_in_memory: { title: "Встроенная память", code: "built_in_memory" },
-  color: { title: "Цвет", code: "color" },
-  speed: { title: "Скорость", code: "speed" },
-  video_resolution: { title: "Разрешение видео", code: "video_resolution" },
-  battery_power: { title: "Питание", code: "battery_power" },
-  construction_type: { title: "Тип конструкции", code: "construction_type" },
-});
